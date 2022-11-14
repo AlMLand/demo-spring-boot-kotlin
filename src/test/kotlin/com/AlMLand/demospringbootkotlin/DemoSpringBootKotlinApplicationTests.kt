497893e7cf169ce30010180a6ae4da257452407a
Alex Morland <morlandalex@googlemail.com>
2022-11-14 23:23:12 +0100

    initial commit

---
+++ src/test/kotlin/com/AlMLand/demospringbootkotlin/DemoSpringBootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.AlMLand.demospringbootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoSpringBootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
