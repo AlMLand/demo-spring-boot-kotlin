5ae9250b0b5de8175e73bfb5876a17a90d7a1bde
Alex Morland <morlandalex@googlemail.com>
2022-11-17 01:51:24 +0100

    part 5

--- src/main/kotlin/com/AlMLand/demospringbootkotlin/DemoSpringBootKotlinApplication.kt
+++ src/main/kotlin/com/AlMLand/demospringbootkotlin/DemoSpringBootKotlinApplication.kt
@@ -1,11 +1,13 @@
 package com.AlMLand.demospringbootkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties::class)
 class DemoSpringBootKotlinApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoSpringBootKotlinApplication>(*args)
+    runApplication<DemoSpringBootKotlinApplication>(*args)
 }
--- src/main/kotlin/com/AlMLand/demospringbootkotlin/controller/http/HtmlController.kt
+++ src/main/kotlin/com/AlMLand/demospringbootkotlin/controller/http/HtmlController.kt
@@ -1,28 +1,29 @@
 package com.AlMLand.demospringbootkotlin.controller
 
+import com.AlMLand.demospringbootkotlin.AppProperties
 import com.AlMLand.demospringbootkotlin.domain.Article
 import com.AlMLand.demospringbootkotlin.domain.User
 import com.AlMLand.demospringbootkotlin.format
 import com.AlMLand.demospringbootkotlin.repository.ArticleRepository
-import com.AlMLand.demospringbootkotlin.toSlug
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
-import java.time.LocalDateTime
-import javax.persistence.ManyToOne
 
 @Controller
-class HtmlController(private val articleRepository: ArticleRepository) {
+class HtmlController(
+    private val articleRepository: ArticleRepository,
+    private val properties: AppProperties
+) {
 
     @GetMapping("/")
     fun blog(model: Model): String {
         //model.addAttribute("title", "Custom title")
         model["title"] = "Custom title"
+        model["banner"] = properties.banner
         model["articles"] = articleRepository.findAllByOrderByAddedAtDesc().map { it.render() }
         return "blog"
     }
@@ -31,7 +32,7 @@ class HtmlController(private val articleRepository: ArticleRepository) {
     fun article(@PathVariable slug: String, model: Model): String {
         val article = articleRepository.findBySlug(slug)
             ?.render()
-            ?:throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
+            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
         model["title"] = article.title
         model["article"] = article
         return "article"
@@ -48,11 +49,11 @@ private fun Article.render() = RenderArticle(
     addedAt.format()
 )
 
-data class RenderArticle (
+data class RenderArticle(
     val slug: String,
     val title: String,
     val headLine: String,
     val content: String,
     val author: User,
     val addedAt: String
-    )
+)
